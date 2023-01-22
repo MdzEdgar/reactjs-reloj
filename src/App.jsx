9c31824041916a7f7574fcb756ab4ec2f72b31e7
Edgar Méndez <mdzredgar@gmail.com>
2023-01-22 16:33:10 -0600

    Reloj terminado

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import { useState } from 'react';
+
+const date = new Date();
+
+function App() {
+  const [seconds, setSeconds] = useState(date.getSeconds())
+  const [minutes, setMinutes] = useState(date.getMinutes())
+  const [hours, setHours] = useState(date.getHours())
+
+  setTimeout(() => {
+    if(seconds !== 59)  return setSeconds(seconds + 1);
+    setSeconds(0)
+
+    if(minutes !== 59) return setMinutes(minutes + 1);
+    setMinutes(0)
+      
+    if(hours !== 23) return setHours(hours + 1);
+    setHours(0)
+  }, 1000)
+
+  return (
+    <div className="App">
+      <h1>{hours < 10 ? 0 : ""}{hours} : {minutes < 10 ? 0 : ""}{minutes} : {seconds < 10 ? 0 : ""}{seconds}</h1>
+    </div>
+  )
+}
+
+export default App
